c6bd419d44b7761cf536b5cc8a0730d9e1f979c7
Marchingo <126391129+Marchingo@users.noreply.github.com>
2023-05-06 19:33:26 +0530

    commit to main

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 let gtnEl = document.getElementById("gtn-el");
 let renderPhotoEl = document.getElementById("renderPhoto");
-let listEl = document.getElementById("list")
+let listEl = document.getElementById("list");
+let numberEL = document.getElementById("number-el");
+let con = [];
+let j
+if (localStorage.getItem("listS")) {
+  for( j=0; j<con.length; j++){
 
+    
+    listEl.innerHTML += `<p><a href='img${localStorage.getItem(con[j])}.jpg' target='_blank' download>l-WallPaper-${localStorage.getItem(con[j])}</a><p>`
+  }
+}
 function getPhoto() {
-  let randomValue = Math.floor(Math.random() * 20) + 21;
+  let randomValue = Math.floor(Math.random() * 3) + 21;
   console.log(randomValue);
   for (let i = 21; i <= randomValue; i++) {
     if (randomValue === i) {
-      let randomcomparision = Math.floor(Math.random() * 20) + 21;
+      let randomcomparision = Math.floor(Math.random() * 3) + 21;
       if (randomValue === randomcomparision) {
-        renderPhotoEl.innerHTML = `<img class='legendry-img' src='img${i}.jpg'>`;
-        renderPhotoEl.innerHTML += `<embed src='winningsound.wav' type='audio/midi' autostart='true' hidden='true' loop='false'></embed>`;
-        listEl.innerHTML += `<p><a href='img${i}.jpg' target='_blank'>l-WallPaper-${i}</a><p>`
+        renderPhotoEl.innerHTML = `<img class='legendry-imgs' src='img${i}.jpg'>`;
+        // renderPhotoEl.innerHTML += `<embed src='winningsound.wav' type='audio/midi' autostart='true' hidden='true' loop='false'></embed>`;
+        listEl.innerHTML += `<p><a href='img${i}.jpg' target='_blank' download>l-WallPaper-${i}</a><p>`;
+        con.push(i) ;
+        localStorage.setItem("listS", con);
+        console.log(localStorage.getItem(listS[2]))
         console.log("legendry");
       } else {
-        renderPhotoEl.innerHTML = `<img class='img-dec' src='img${i}.jpg'>`;
+        renderPhotoEl.innerHTML = `<img class='img-doc' src='img${i}.jpg'>`;
       }
     } else {
       renderPhotoEl.textContent = "Sorry there is some technical error";
     }
   }
+}
+
+let count = 0;
+function list_of_tap() {
+  count++;
+  numberEL.innerHTML = `${count}`
 }
